931e8c0523eba44058fcc08817bc5f2a3771a7fc
Vinnie Moth <vinniemoth@gmail.com>
2025-07-04 13:54:43 -0300

    FEAT: Implemented DI on Auth routes/services

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,78 +1,34 @@
 import express from "express";
-import bcrypt from "bcryptjs";
-import { PrismaClient } from "@prisma/client";
-import jwt from "jsonwebtoken";
 
-const prisma = new PrismaClient();
-const router = express.Router();
+function setupAuthRoutes(authServices) {
+  const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(username, email, password);
-  try {
-    const newUser = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password: hashedPassword,
-      },
-    });
-    return res
-      .status(201)
-      .json({ message: "Usuário criado com sucesso!", newUser });
-  } catch (err) {
-    return res.status(500).json({ message: "Erro interno do servidor." });
-  }
-});
-
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({ message: "Email e senha obrigatórios" });
-  }
-
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-
-    if (!user) {
-      return res.status(401).json({ message: "Credenciais Inválidas" });
+  router.post("/", async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+      const newUser = authServices.createAccount(username, email, password);
+      res.status(200).json(newUser);
+    } catch (err) {
+      return res.status(500).json({ message: "Erro interno do servidor." });
     }
-    const passwordIsValid = bcrypt.compareSync(password, user.password);
+  });
 
-    if (!passwordIsValid) {
-      return res.status(401).json({
-        message: "Credenciais inválidas",
-      });
+  router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const user = authServices.login(email, password);
+      res.status(200).json(user);
+    } catch (err) {
+      return res.status(500).json({ message: "Erro interno do servidor." });
     }
-    const tokenPayload = {
-      id: user.id,
-      email: user.email,
-    };
-    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    return res.status(200).json({
-      message: "Login feito com sucesso!",
-      user: { id: user.id, email: user.email },
-      token,
-    });
-  } catch (err) {
-    console.error("Erro no login do usuário", err);
-    return res.status(500).json({
-      message: "Ocorreu um erro interno no servidor durante o login.",
-    });
-  }
-});
+  });
 
-router.post("/logout", (req, res) => {
-  console.log("Requisição de logout recebida");
-  return res.status(200).json({ message: "Logout feito com sucesso!" });
-});
+  router.post("/logout", async (req, res) => {
+    console.log("Requisição de logout recebida");
+    return res.status(200).json({ message: "Logout feito com sucesso!" });
+  });
 
-export default router;
+  return router;
+}
+
+export default setupAuthRoutes;
